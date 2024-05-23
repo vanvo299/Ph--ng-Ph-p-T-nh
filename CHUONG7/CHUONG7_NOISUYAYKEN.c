32f2fc4728cac755a0349b921801da6a7a2f784a
vanvo299 <luongvanvo29@gmail.com>
2024-05-23 17:36:57 +0700

    Update CHUONG7_NOISUYAYKEN.c

--- CHUONG7/CHUONG7_NOISUYAYKEN.c
+++ CHUONG7/CHUONG7_NOISUYAYKEN.c
@@ -13,7 +13,7 @@ void Nhap(float x[], float y[], int n)
         scanf("%f", &y[i]);
     }
 }
-
+// bảng nội suy ayken 1
 float bangNoiSuyAyken(float x[], float y[], int n, float c)
 {
     float w = 1, s = 0;
@@ -27,6 +27,17 @@ float bangNoiSuyAyken(float x[], float y[], int n, float c)
     }
     return w*s;
 }
+
+// bảng nội suy ayken 2
+float bangNoiSuyAyKen2(float x[], float y[], int n, float c) {
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) { // j < n thay vì j <= n
+            y[j] = (y[i] * (x[j] - c) - y[j] * (x[i] - c)) / (x[j] - x[i]);
+        }
+    }
+    return y[n-1]; // Trả về giá trị cuối cùng của mảng y
+}
+
 int main()
 {
     float x[10], y[10], c;
@@ -37,6 +48,9 @@ int main()
     scanf("%f", &c);
     Nhap(x, y, n);
     float result = bangNoiSuyAyken(x, y, n, c);
-    printf("f(%.1f) = %f\n", c, result);
+    printf("f(%.1f) = L%d(%.1f) = %f\n", c, n - 1, c, result);
+    float result2 = bangNoiSuyAyKen2(x, y, n, c);
+    printf("f(%.1f) = L%d(%.1f) = %f\n", c, n - 1, c, result2);
+
     return 0;
 }
